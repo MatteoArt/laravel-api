b6ab7e145021a4e60cac1ca05368def4ac7e0ca9
MatteoArt <mattrim8@gmail.com>
2023-10-08 19:11:02 +0200

    migration, controller project, model

---
+++ app/Http/Controllers/Admin/ProjectController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ProjectController extends Controller
+{
+    public function index() {
+
+    }
+
+    public function show($id) {
+
+    }
+
+    public function create() {
+
+    }
+
+    public function store() {
+        
+    }
+}
---
+++ database/migrations/2023_10_08_160411_create_projects_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('projects', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 150)->unique();
+            $table->text('description');
+            $table->string('img', 200);
+            $table->json('languages');
+            $table->string('repository', 1000);
+            $table->string('page_project', 1000)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('projects');
+    }
+};
