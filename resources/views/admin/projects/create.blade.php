69cbf8b5a7289f1d6a05def108c1886a2b473ac2
MatteoArt <mattrim8@gmail.com>
2023-10-09 21:51:28 +0200

    create

--- resources/views/admin/projects/create.blade.php
+++ resources/views/admin/projects/create.blade.php
@@ -9,11 +9,31 @@
             <input name="title" id="titolo" type="text" class="form-control" placeholder="Nome del progetto"
                 aria-label="Nome del progetto" aria-describedby="basic-addon1">
         </div>
-        <div class="input-group">
+        <div class="input-group mb-3">
             <span class="input-group-text">
                 <label for="descrizione">Descrizione</label>
             </span>
             <textarea name="description" id="descrizione" class="form-control" aria-label="With textarea"></textarea>
+        </div>
+        <div class="input-group mb-3">
+            <span class="input-group-text">
+                <label for="linguaggi">Linguaggi</label>
+            </span>
+            <textarea name="languages" id="linguaggi" class="form-control" aria-label="With textarea"></textarea>
+        </div>
+        <div class="input-group mb-3">
+            <span class="input-group-text" id="basic-addon1">
+                <label for="repo">URL Repository</label>
+            </span>
+            <input name="repository" id="repo" type="text" class="form-control" placeholder="URL Repository"
+                aria-label="URL Repository" aria-describedby="basic-addon1">
+        </div>
+        <div class="input-group mb-3">
+            <span class="input-group-text" id="basic-addon1">
+                <label for="pagina_progetto">URL progetto</label>
+            </span>
+            <input name="page_project" id="pagina_progetto" type="text" class="form-control" placeholder="URL Progetto"
+                aria-label="URL Progetto" aria-describedby="basic-addon1">
         </div>
         <button type="submit" class="btn btn-primary">Aggiungi</button>
     </form>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -49,6 +49,9 @@
                         <li class="nav-item">
                             <a class="nav-link" href="{{ route('admin.projects.index') }}">Progetti</a>
                         </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('admin.projects.create') }}">Aggiungi progetto</a>
+                        </li>
                     </ul>
 
                     <!-- Right Side Of Navbar -->
