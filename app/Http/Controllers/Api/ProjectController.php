146c403bdefd30f531f1c8bae972ea807190a723
MatteoArt <mattrim8@gmail.com>
2023-10-16 17:10:15 +0200

    milestone 1 e 2

---
+++ app/Http/Controllers/Api/ProjectController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+use App\Models\Project;
+
+class ProjectController extends Controller
+{
+    public function index() {
+        //per ora leggo tutti i dati senza paginazione tanto ho solo 6 record nel db
+        $projects = Project::all();
+
+        return response()->json([
+            'results' => $projects
+        ]);
+    }
+}
