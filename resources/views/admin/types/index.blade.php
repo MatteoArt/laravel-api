2f1291ce808990551b521aec759f84415f363c4f
MatteoArt <mattrim8@gmail.com>
2023-10-29 22:21:39 +0100

    add view index types

--- resources/views/admin/types/index.blade.php
+++ resources/views/admin/types/index.blade.php
@@ -1,5 +1,36 @@
 @extends('layouts.app')
 
 @section('content')
-
+<table class="table table-striped">
+    <thead>
+        <tr>
+            <th scope="col">ID</th>
+            <th scope="col">Nome</th>
+            <th scope="col">Descrizione</th>
+            <th scope="col">Colore</th>
+        </tr>
+    </thead>
+    <tbody>
+        @foreach ($types as $type)
+            <tr>
+                <th scope="row"> {{ $type->id }} </th>
+                <td> {{ $type->name }} </td>
+                <td>
+                    @if ($type->description)
+                        <span> {{ $type->description }} </span>
+                    @else
+                        <span>//</span>
+                    @endif
+                </td>
+                <td>
+                    @if ($type->color)
+                        <span> {{ $type->color }} </span>
+                    @else
+                        <span>//</span>
+                    @endif
+                </td>
+            </tr>
+        @endforeach
+    </tbody>
+</table>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\Admin\ProjectController;
+use App\Http\Controllers\TypeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -28,6 +29,7 @@ Route::get('/admin', function () {
 Route::middleware(['auth', 'verified'])
 ->name('admin.')
 ->prefix('admin')->group(function () {
+    /* Projects */
     //CREATE
     Route::get('/create', [ProjectController::class, 'create'])->name('projects.create');
     Route::post('/projects', [ProjectController::class, 'store'])->name('projects.store');
@@ -42,6 +44,11 @@ Route::middleware(['auth', 'verified'])
 
     //DELETE
     Route::delete('/projects/{id}', [ProjectController::class, 'destroy'])->name('projects.destroy');
+
+    /* Types */
+    //READ
+    Route::get('/types', [TypeController::class, 'index'])->name('types.index');
+
 });
 
 
